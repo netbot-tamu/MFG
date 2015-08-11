74002dbc5b9768c1d02ffdc0b59669d46600d63e
Madison Treat <madison.treat@tamu.edu>
2015-08-11 09:59:20 -0500

    Updated licenses to comply with Qt5's LGPL v3; closes #1

--- src/mfgcore/export.cpp
+++ src/mfgcore/export.cpp
@@ -5,7 +5,7 @@
 //  Netbot Laboratory, Texas A&M University, USA
 //
 //  This program is free software; you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
+//  it under the terms of the GNU Lesesr General Public License as published by
 //  the Free Software Foundation; either version 3 of the License, or
 //  (at your option) any later version.
 //
